ff08aa05f0b0e01953131c651d00cf97d21a943c
Michael Gratton <gratton@metsci.com>
2019-02-16 11:19:49 -0500

    Syslog sink

---
+++ syslog/test/main.cpp
@@ -0,0 +1,93 @@
+#include <g3log/g3log.hpp>
+#include <g3log/logworker.hpp>
+#include <iomanip>
+#include <thread>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <g3log/syslogsink.hpp>
+
+#include <g3log/loglevels.hpp>
+// const LEVELS DBUG{G3LOG_DEBUG.value+1, "DBUG"}; // defines already
+// const LEVELS INFO{INFO.value+1, {"INFO"}}; // defined already
+const LEVELS NOTE{(INFO.value + WARNING.value)/2, {"NOTE"}};
+const LEVELS WARN{WARNING.value, {"WARN"}};
+const LEVELS ERRR{WARN.value+1, {"ERRR"}};
+const LEVELS FATL{FATAL.value, {"FATL"}};
+
+
+std::string myFormat(const g3::LogMessage& msg)
+{
+    static std::stringstream ss;
+    ss.str("");
+    ss << msg.timestamp() << " " 
+        << msg.level() << " " 
+        << msg.file() << ":" << msg.function() << ":" << msg.line();
+    ss << "\t| ";                 
+    return ss.str();
+}
+
+std::string sysFormat(const g3::LogMessage& msg)
+{
+    static std::stringstream ss;
+    ss.str("");
+    ss << "[" << msg.level() << " " 
+       << msg.file() << ":" << msg.function() << ":" << msg.line();
+    ss << "] ";                 
+    return ss.str();
+}
+
+    
+struct log_singleton {
+private:
+    std::unique_ptr<g3::LogWorker> worker;
+public:
+    static log_singleton& get() {
+        static std::unique_ptr<log_singleton> s_instance;
+        if (!s_instance)
+            s_instance = std::make_unique<log_singleton>();
+        return *s_instance;
+    }
+    
+    static void start() {
+        auto sink = std::make_unique<g3::SyslogSink>("g3syslog");
+        sink->setFormatter(&sysFormat);
+        sink->echoToStderr();
+        auto& inst = log_singleton::get();
+        inst.worker = g3::LogWorker::createLogWorker();
+        inst.worker->addSink(std::move(sink), &g3::SyslogSink::syslog);    
+        g3::initializeLogging(inst.worker.get());
+    }
+};
+
+int main(int argc, char** argv)
+{
+    log_singleton::start();
+        
+
+    double pi_d = 3.1415926535897932384626433832795;
+    float pi_f = 3.1415926535897932384626433832795f;
+    
+    LOGF(INFO, "Hi log %d", 123);
+    LOG(INFO) << "Test SLOG INFO";
+    LOG(G3LOG_DEBUG) << "Test SLOG DEBUG";
+    LOG(INFO) << "one: " << 1;
+    LOG(INFO) << "two: " << 2;
+    LOG(INFO) << "one and two: " << 1 << " and " << 2;
+    LOG(G3LOG_DEBUG) << "float 2.14: " << 1000 / 2.14f;
+    LOG(G3LOG_DEBUG) << "pi double: " << pi_d;
+    LOG(G3LOG_DEBUG) << "pi float: " << pi_f;
+    LOG(G3LOG_DEBUG) << "pi float (width 10): " << std::setprecision(10) << pi_f;
+    LOGF(INFO, "pi float printf:%f", pi_f);
+    LOG_IF(INFO, 1 != 2) << "Turns out that 1 != 2";
+    
+    // Custom levels
+    LOG(DBUG) << "debug level log";
+    LOG(INFO) << "info level log";    
+    LOG(NOTE) << "Notice level log";
+    LOG(WARN) << "Warning level log";
+    LOG(ERRR) << "error level log";
+    LOG(FATL) << "fatal level log";
+}
+
+
