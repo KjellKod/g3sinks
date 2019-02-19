f1b21e0e84fd6e0c69f8ddd118c8304a37547134
Kjell Hedstrom <Hedstrom@KjellKod.cc>
2019-02-18 19:31:42 -0700

    try gcc first

--- scripts/buildAndRunTests.sh
+++ scripts/buildAndRunTests.sh
@@ -10,7 +10,7 @@ cd g3log
 unzip -o 3rdParty/gtest/gtest-1.7.0.zip -d 3rdParty/gtest
 mkdir -p  build_travis
 cd build_travis
-cmake  -DCMAKE_CXX_FLAGS=-std=c++14 -DADD_G3LOG_UNIT_TEST=ON ..
+cmake  -DADD_G3LOG_UNIT_TEST=ON ..
 make -j && make install
 
 
