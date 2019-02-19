8d47afebd05d77864dd540fd3db87624b9247819
Kjell Hedström <hedstrom@kjellkod.cc>
2019-02-18 18:09:08 -0700

    Cmake fix osx (#66)
    * Syslog sink
    
    * Add details to readme and comments to example
    
    * Updated for compile error (class/struct), OSX support, not test --> an example (test should be gtest), README
    
    * Thanks to https://github.com/KjellKod/g3log/pull/294 @BenjaminBeichler
    
    * Thanks to @BenjaminBeichler https://github.com/KjellKod/g3log/pull/294 to show the OSX build simplification

---
+++ syslog/src/syslogsink.cpp
@@ -0,0 +1,69 @@
+#include "g3log/logmessage.hpp"
+#include "g3log/syslogsink.hpp"
+#include <syslog.h>
+
+namespace g3
+{
+
+void SyslogSink::setLevelMap(std::map<int, int> const& m)
+{
+    _levelMap = m;
+}
+
+void SyslogSink::setLevel(LogLevel level, int syslevel)
+{
+    _levelMap[level.value] = syslevel;
+}
+
+void SyslogSink::echoToStderr()
+{
+    _option |= LOG_PERROR;
+}
+
+// The actual log receiving function
+void SyslogSink::syslog(LogMessageMover message)
+{
+    if(_firstEntry)
+    {
+        openlog(_identity.c_str(), _option, _facility);
+        if(!_header.empty())
+        {
+            ::syslog(LOG_NOTICE, "%s", _header.c_str());
+        }
+        _firstEntry = false;
+    }
+    int level = priority(message.get()._level);
+    ::syslog(level, "%s", message.get().toString(_log_details_func).c_str());
+}
+
+int SyslogSink::priority(LogLevel level)
+{
+    auto left = _levelMap.lower_bound(level.value);
+    if(left == _levelMap.end())
+    {
+        return LOG_DEBUG;
+    }
+    return left->second;
+}
+
+SyslogSink::SyslogSink(const char* identity)
+    : _log_details_func(&LogMessage::DefaultLogDetailsToString)
+    , _identity(identity)
+    , _facility(LOG_USER)
+    , _option(LOG_PID)
+    , _header("")
+    , _firstEntry(true)
+{
+    _levelMap[G3LOG_DEBUG.value] = LOG_DEBUG;
+    _levelMap[INFO.value] = LOG_INFO;
+    _levelMap[(INFO.value + WARNING.value)/2] = LOG_NOTICE;
+    _levelMap[WARNING.value] = LOG_WARNING;
+    _levelMap[FATAL.value] = LOG_CRIT;
+}
+
+SyslogSink::~SyslogSink()
+{
+    ::closelog();
+}
+
+}
